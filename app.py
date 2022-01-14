9e5d518005e1279afe1f7634464e74b5b2446cb4
김정호 <43710866+fancyers@users.noreply.github.com>
2022-01-14 21:31:22 +0900

    즐겨찾기 기능 구현
    빈 아이콘을 누르면 찬 아이콘이 되며 특정 식당이 즐겨찾기에 추가되고, 다시 누르면 즐겨찾기에서 제외된다.
    즐겨찾기에 추가되면 지도에 뜨는 마커의 색이 빨간색으로 바뀌는 기능도 구현했다.

---
+++ app.py
@@ -0,0 +1,35 @@
+from flask import Flask, render_template, request, jsonify, redirect, url_for
+from pymongo import MongoClient
+
+
+app = Flask(__name__)
+
+client = MongoClient('3.34.44.93', 27017, username="sparta", password="woowa")
+db = client.dbsparta_plus_week3
+
+
+@app.route('/')
+def main():
+    return render_template("index.html")
+
+@app.route('/matjip', methods=["GET"])
+def get_matjip():
+    # 맛집 목록을 반환하는 API
+    matjip_list = list(db.matjips.find({}, {'_id': False}))
+    # matjip_list 라는 키 값에 맛집 목록을 담아 클라이언트에게 반환합니다.
+    return jsonify({'result': 'success', 'matjip_list': matjip_list})
+
+@app.route('/like', methods=["POST"])
+def like():
+    title_receive = request.form['title_give']
+    status_receive = request.form['status_give']
+
+    if status_receive == 'like':
+        db.matjips.update_one({'title': title_receive}, {'$set': {'liked': True}})
+    else:
+        db.matjips.update_one({'title': title_receive}, {'$set': {'liked': False}})
+
+    return jsonify({'result': 'success'})
+
+if __name__ == '__main__':
+    app.run('0.0.0.0', port=5000, debug=True)
